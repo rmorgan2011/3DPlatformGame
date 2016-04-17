1bfa0792c27f408198b6c5fa4fccd042133cb73c
Regan Townsend <townsenr17@up.edu>
2016-04-17 10:16:41 -0700

    added changeSize script so that some platforms will grow and shrink continuously over time on level 5

---
+++ Assets/Scripts/Cubes/changeSize.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeSize : MonoBehaviour {
+    public float scaleRate = .006f;
+    public float minScale = .1f;
+    public float maxScale =.9f;
+
+    public bool scaleOrMove;
+
+    void ApplyScaleRate()
+    {
+        transform.localScale += Vector3.one * scaleRate;
+    }
+
+    void main()
+    {
+         //if we exceed the defined range then correct the sign of scaleRate.
+        if (transform.localScale.x < minScale)
+        {
+            scaleRate = Mathf.Abs(scaleRate);
+        }
+        else if (transform.localScale.x > maxScale)
+        {
+            scaleRate = -Mathf.Abs(scaleRate);
+        }
+        ApplyScaleRate();
+    }
+    
+    void Update()
+    {
+       
+        main();
+  
+    }
+
+}
